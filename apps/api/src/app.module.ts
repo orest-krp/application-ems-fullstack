fb4716e0834c8a00e1bf14412fac8a757695c6a4
orest-krp <albor1veras@gmail.com>
2026-03-10 19:52:42 +0100

    Fix typos

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { envValidationSchema } from './config.ts/env.validation.shema';
-import base from './config.ts/base';
+import { envValidationSchema } from './config/env.validation.shema';
+import base from './config/base';
 import { PrismaService } from './prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
